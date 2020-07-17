5702fb94454344170cf48a23696954f656336cb5
kimdain <cd8025@nate.com>
2020-07-17 14:24:00 +0900

    commit 테스트를 위한 commit

--- dp/Q_11726_2xnTileing.java
+++ dp/Q_11726_2xnTileing.java
@@ -1,12 +1,13 @@
 package dp;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Q_11726_2xnTileing {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int num = Integer.parseInt(br.r)
+        int num = Integer.parseInt(br.readLine());
 
     }
 }
