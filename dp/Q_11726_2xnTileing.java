ffeca4b470adb90c5a35d0249b48e1de8f861275
kimdainn <cd8025@nate.com>
2020-08-12 23:02:48 +0900

    2xn tileing

--- dp/Q_11726_2xnTileing.java
+++ dp/Q_11726_2xnTileing.java
@@ -8,6 +8,22 @@ public class Q_11726_2xnTileing {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int num = Integer.parseInt(br.readLine());
+        int[] array = new int[1000];
+
+        array[0] = 0;
+        array[1] = 2;
+        array[2] = 2;
+        array[3] = 2;
+
+        for(int i=4 ; i<=num ; i++){
+            if(i%2 == 0) {
+                array[i] = array[i - 2] * 2 * 2;
+                System.out.println(i+" : "+array[i]);
+            }
+            else
+                array[i] = array[i-1] * (i/2+1) * 2;
+        }
+        System.out.println(array[num]%10007);
 
     }
 }
