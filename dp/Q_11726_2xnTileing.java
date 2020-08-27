5ad0acec16e0bcc64f032d5182f84c588b28fd25
김다인 <dainkim@sparrowfasoo.com>
2020-08-27 15:19:49 +0900

    dp 11726 풀기

--- dp/Q_11726_2xnTileing.java
+++ dp/Q_11726_2xnTileing.java
@@ -8,22 +8,18 @@ public class Q_11726_2xnTileing {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int num = Integer.parseInt(br.readLine());
-        int[] array = new int[1000];
+        int[] a = new int[1001];
 
-        array[0] = 0;
-        array[1] = 2;
-        array[2] = 2;
-        array[3] = 2;
+        a[0] = 1;
+        a[1] = 1;
 
-        for(int i=4 ; i<=num ; i++){
-            if(i%2 == 0) {
-                array[i] = array[i - 2] * 2 * 2;
-                System.out.println(i+" : "+array[i]);
-            }
-            else
-                array[i] = array[i-1] * (i/2+1) * 2;
+        for(int i=2 ; i<=num ; i++){
+            // a[n-2]의 사각형 오른쪽에 누워진 직사각형 2개가 붙음
+            // a[n-1]의 사각형 오른쪽에 세워진 직사각형 1개가 붙음
+            a[i] = a[i - 2] + a[i - 1];
+            a[i] %= 10007; // 왜 최종 값에다 해주지 않고 반복분안에서 해줘야하는거지..?
         }
-        System.out.println(array[num]%10007);
+        System.out.println(a[num]);
 
     }
 }
