e440859e5607a6b340f9163655291539328a2b95
kimdainn <cd8025@nate.com>
2020-09-22 20:31:27 +0900

    9/22 commit

--- mathmatics/Q_11005_MathSystem.java
+++ mathmatics/Q_11005_MathSystem.java
@@ -1,9 +1,11 @@
 package mathmatics;
 
 import java.io.*;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Q_11005_MathSystem {
+    static Stack<String> stack = new Stack();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -12,10 +14,45 @@ public class Q_11005_MathSystem {
         int n = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
 
-        if(b>= 11 && b<15)
-
+        solution(n, b);
+        while(!stack.isEmpty()) {
+            bw.write(stack.pop());
+        }
 
         br.close();
         bw.close();
+    }
+
+    public static void solution(int n, int b) {
+
+        int share = n;
+        int rest = 0;
+
+        while (share >= b) {
+            rest = share % b;
+            share = share / b;
+
+            char c = (char) (b + 55);
+            if(b>=10 && b<36)
+                stack.add(String.valueOf(c));
+//            else if(b==36) {
+//                c = (char) (b + 54);
+//                stack.add(String.valueOf(c));
+//            }
+            else
+                stack.add(String.valueOf(rest));
+
+            if(share < b){
+                if(b>=10 && b<36)
+                    stack.add(String.valueOf(c));
+                else if(b==36) {
+                    c = (char) (b + 54);
+                    stack.add(String.valueOf(c));
+                }
+                else
+                    stack.add(String.valueOf(share));
+            }
+        }
+
     }
 }
