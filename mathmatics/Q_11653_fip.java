5f1928c150a4729e24bbff00b0102e328dba0261
kimdainn <cd8025@nate.com>
2020-10-11 11:27:16 +0900

    conflict

---
+++ mathmatics/Q_11653_fip.java
@@ -0,0 +1,33 @@
+package mathmatics;
+
+import java.io.*;
+
+public class Q_11653_fip {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int num = Integer.parseInt(br.readLine());
+
+        int count = 0;
+
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) {
+                count++;
+            }
+        }
+
+        br.close();
+        bw.close();
+    }
+
+    public static void solution(int n) {
+        int share = 0;
+        int rest = 0;
+
+        while(share !=0){
+            share = n/2;
+            rest = n%2;
+        }
+    }
+}
--- mathmatics/Q_6588_GoldBach.java
+++ mathmatics/Q_6588_GoldBach.java
@@ -1,6 +1,8 @@
 package mathmatics;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Q_6588_GoldBach {
     public static void main(String[] args) throws IOException {
@@ -15,6 +17,27 @@ public class Q_6588_GoldBach {
 
         br.close();
         bw.close();
+    }
+
+    public static void solution(int n){
+        int a = 0;
+        int b = 0;
+
+        int count = 0;
+        List list = new ArrayList<Integer>();
+
+
+        for (int i = 1; i < n; i++) {
+            for(int j=0 ; j<=n ; j++) {
+                if (n % j == 0)
+                    count++;
+            }
+            if(count == 2){
+                list.add(i);
+            }
+
+
+        }
 
     }
 }
