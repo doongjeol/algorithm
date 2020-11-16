6bdcabdbc52191a6c06cec67a2e9b18817f12c80
김다인 <dainkim@sparrowfasoo.com>
2020-11-16 19:03:35 +0900

    11월 16일

--- baekjoon/dp/Q_9465_Stricker.java
+++ baekjoon/dp/Q_9465_Stricker.java
@@ -4,17 +4,19 @@ import java.util.StringTokenizer;
 
 public class Q_9465_Stricker {
     public static int solution(int[][] sticker){
-        int[][] a = new int[3][sticker.length];
+        int length = sticker[0].length;
+        int[][] a = new int[3][length+1];
         int max = 0;
 
-        for(int i=1 ; i<sticker.length ; i++){
-            a[0][i] = Math.max(a[0][i - 1], Math.max(a[1][i - 1], a[2][i - 1]));
-            a[1][i] = Math.max(a[0][i - 1], a[2][i - 1]) + sticker[0][i];
-            a[2][i] = Math.max(a[0][i - 1], a[1][i - 1]) + sticker[1][i];
-            max = Math.max(a[0][i], Math.max(a[0][i], a[2][i]));
-
+        for(int col=0 ; col<length; col++){
+            a[0][col] = Math.max(a[0][col - 1], Math.max(a[1][col - 1], a[2][col - 1]));
+            a[1][col] = Math.max(a[0][col - 1], a[2][col - 1]) + sticker[0][col - 1];
+            a[2][col] = Math.max(a[0][col - 1], a[1][col - 1]) + sticker[1][col - 1];
+            max = Math.max(a[0][col], Math.max(a[0][col], a[2][col]));
         }
 
+
+        print(a);
         return max;
 
     }
@@ -26,16 +28,25 @@ public class Q_9465_Stricker {
         for (int i = 0; i < t; i++) {
             int n = Integer.parseInt(br.readLine()); // n열
             StringTokenizer st = new StringTokenizer(br.readLine());
+            StringTokenizer st2 = new StringTokenizer(br.readLine());
             int[][] sticker = new int[2][n]; // 스티커 점수
-            for(int row = 0 ; row < 2 ; row ++) {
-                for (int col = 0; col < n; i++) {
-                    sticker[row][col] = Integer.parseInt(st.nextToken());
-                }
+            for (int col = 0; col < n; col++) {
+                sticker[0][col] = Integer.parseInt(st.nextToken());
+                sticker[1][col] = Integer.parseInt(st2.nextToken());
             }
 
             bw.write(solution(sticker) + "\n");
         }
         br.close();
         bw.close();
+    }
+
+    public static void print(int[][] array) {
+        for(int i=0 ; i<array.length ; i++) {
+            for(int j=0 ; j<array[0].length ; j++) {
+                System.out.print(array[i][j]+" ");
+            }
+            System.out.println();
+        }
     }
 }
