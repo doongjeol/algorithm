e1156370a8147fe0b95b46dcfbae18f1169b521f
김다인 <dainkim@sparrowfasoo.com>
2020-11-17 17:53:55 +0900

    11월 17일

--- baekjoon/dp/Q_2294_Coin22.java
+++ baekjoon/dp/Q_2294_Coin22.java
@@ -1,6 +1,5 @@
 package baekjoon.dp;
 import java.io.*;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 //2020년 11월 16일 혼자힘으로 풀어보기
@@ -9,34 +8,33 @@ public class Q_2294_Coin22 {
         int[] a = new int[k + 1];
         
         for(int i=1; i<=k; i++)
-            a[i] = 100001;
+            a[i] = 10001;
 
         a[0] = 0;
         for (int i = 0; i < coin.length; i++) {
-            for(int j=coin[i] ; j<k ; j++)
+            for(int j=coin[i] ; j<=k ; j++)
             a[j] = Math.min(a[j], a[j - coin[i]] + 1);
         }
 
-        if(coin[k] == 10001)
-            coin[k] = -1;
+        if(a[k] == 10001)
+            a[k] = -1;
 
-        return coin[k];
+        return a[k];
     }
     public static void main (String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken()); // 동전의 가치
+        int k = Integer.parseInt(st.nextToken()); // 가치의 합
 
         int[] coin = new int[n];
         for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            coin[i] = Integer.parseInt(st.nextToken());
+            coin[i] = Integer.parseInt(br.readLine());
         }
 
-        bw.write(solution(coin,k));
+        bw.write(solution(coin,k)+"\n");
 
         br.close();
         bw.close();
