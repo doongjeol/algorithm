0592fbfdc78de9e68dd8b37f37824c9a6e9c05cb
kimdainn <cd8025@nate.com>
2020-11-24 21:20:43 +0900

    11월 24일

--- baekjoon/dp/Q_1699_SumOfSquareNumber.java
+++ baekjoon/dp/Q_1699_SumOfSquareNumber.java
@@ -12,24 +12,15 @@ public class Q_1699_SumOfSquareNumber {
         int[] a = new int[n + 1];
         a[1] = 1;
         int k = 1;
-        int temp = 1;
-        int index = 1;
         for (int i = 2; i < n + 1; i++) {
-            k = temp;
             if(i == square[k+1]) {
                 a[i] = 1;
                 k++;
-                temp = k;
             } else if(i > square[k]) {
-                while(i > square[k] && k > 0) {
-                    a[i] += a[square[k]];
-                    index += square[k];
-                    k--;
-                }
-                k++;
-                a[i] += a[i - index];
+                a[i] = a[square[k]] + a[i-square[k]];
+                if(i == square[k-1] * 2)
+                    a[i] = 2;
             }
-            System.out.println("a["+i+"] : "+a[i]);
         }
 
         return a[n];
