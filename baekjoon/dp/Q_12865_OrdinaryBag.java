5b19d802df246b77153121853941eddda67e7771
kimdainn <cd8025@nate.com>
2020-12-03 22:06:25 +0900

    12월 3일

--- baekjoon/dp/Q_12865_OrdinaryBag.java
+++ baekjoon/dp/Q_12865_OrdinaryBag.java
@@ -5,13 +5,13 @@ import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class Q_12865_OrdinaryBag {
-    public static int solution(int k, int[][] wv) {
+    public static int solution(int k, int[] w, int[] v) {
 // w -> k , v 최대
         int[] a = new int[k+1];
 
-        for (int i = 0; i < wv.length; i++) {
-            for (int j = wv[i][0]; j <= k; j = wv[i][0]) {
-                a[j] = Math.max(a[j], a[j - wv[i][0]] + wv[i][1]);
+        for (int i = 0; i < w.length; i++) {
+            for (int j = w[i]; j <= k; j++) {
+                a[j] = Math.max(a[j], a[j - w[i]] + v[i]);
             }
         }
 
@@ -25,24 +25,15 @@ public class Q_12865_OrdinaryBag {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
-        int[][] wv = new int[n][2];
+        int[] w = new int[n];
+        int[] v = new int[n];
         for(int i=0 ; i<n ; i++){
             st = new StringTokenizer(br.readLine());
-            wv[i][0] = Integer.parseInt(st.nextToken()); // 무게
-            wv[i][1] = Integer.parseInt(st.nextToken()); // 가치
+            w[i] = Integer.parseInt(st.nextToken()); // 무게
+            v[i] = Integer.parseInt(st.nextToken()); // 가치
         }
 
-        Arrays.sort(wv, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                if(o1[0] == o2[0])
-                    return Integer.compare(o1[1], o2[1]);
-                else
-                    return Integer.compare(o1[0], o2[0]);
-            }
-        });
-//        print(wv);
-        bw.write(solution(k,wv)+"\n");
+        bw.write(solution(k,w,v)+"\n");
 
         br.close();
         bw.close();
