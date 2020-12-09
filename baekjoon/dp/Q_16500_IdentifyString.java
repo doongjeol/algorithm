3b6f50a862127df8e010b7c0d81318b8927544a2
김다인 <dainkim@sparrowfasoo.com>
2020-12-09 14:58:59 +0900

    12월 9일

--- baekjoon/dp/Q_16500_IdentifyString.java
+++ baekjoon/dp/Q_16500_IdentifyString.java
@@ -2,7 +2,9 @@ package baekjoon.dp;
 import java.io.*;
 
 public class Q_16500_IdentifyString {
-    public static void solution(){
+    public static int solution(String S, int n, String[] A){
+
+        return 1;
 
     }
     public static void main (String[] args) throws IOException {
@@ -15,6 +17,8 @@ public class Q_16500_IdentifyString {
         for (int i = 0; i < n; i++) {
             A[i] = br.readLine();
         }
+
+        bw.write(solution(S,n,A)+"\n");
 
         br.close();
         bw.close();
