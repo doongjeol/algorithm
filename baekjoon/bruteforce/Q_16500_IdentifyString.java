279b28281b32c586782db6079d380b8e848d45c5
kimdainn <cd8025@nate.com>
2020-12-15 22:11:10 +0900

    12월 15일

--- baekjoon/bruteforce/Q_16500_IdentifyString.java
+++ baekjoon/bruteforce/Q_16500_IdentifyString.java
@@ -2,12 +2,36 @@ package baekjoon.bruteforce;
 import java.io.*;
 
 public class Q_16500_IdentifyString {
-    public static int solution(String S, int n, String[] A){
-        for (int i = 0; i < S.length(); i++) {
+    public static int solution(String S, String[] A){
+        int[] a = new int[S.length()];
 
+        for(int j=0 ; j<A.length ; j++) {
+            String aStr = A[j]; // A의 j번째 문자열
+            for(int k=0 ; k<aStr.length() ; k++){ // A 문자열
+                for(int position=0 ; position<S.length() ; position++){
+                    char sCur = S.charAt(position);
+
+                    System.out.println(aStr.charAt(k));
+
+                    if(S.contains(aStr)) {
+                        if (sCur == aStr.charAt(k)) {
+                            if(position == 0)
+//                                System.out.println(sCur +" " +aStr.charAt(k));
+                            a[position] = 1;
+                        }
+                    }
+                }
+
+            }
         }
 
-        return 1;
+        for (int i = 0; i < a.length; i++) {
+            if(a[i] == 0) {
+                a[a.length - 1] = 0;
+                break;
+            }
+        }
+        return a[a.length-1];
 
     }
     public static void main (String[] args) throws IOException {
@@ -21,7 +45,7 @@ public class Q_16500_IdentifyString {
             A[i] = br.readLine();
         }
 
-        bw.write(solution(S,n,A)+"\n");
+        bw.write(solution(S,A)+"\n");
 
         br.close();
         bw.close();
