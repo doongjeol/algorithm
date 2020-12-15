6d275b988318fae9fa82688f531051a462ff4ae5
kimdainn <cd8025@nate.com>
2020-12-15 13:02:08 +0900

    12월 15일

---
+++ baekjoon/bruteforce/Q_16500_IdentifyString.java
@@ -0,0 +1,29 @@
+package baekjoon.bruteforce;
+import java.io.*;
+
+public class Q_16500_IdentifyString {
+    public static int solution(String S, int n, String[] A){
+        for (int i = 0; i < S.length(); i++) {
+
+        }
+
+        return 1;
+
+    }
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String S = br.readLine();
+        int n = Integer.parseInt(br.readLine());
+        String[] A = new String[n];
+        for (int i = 0; i < n; i++) {
+            A[i] = br.readLine();
+        }
+
+        bw.write(solution(S,n,A)+"\n");
+
+        br.close();
+        bw.close();
+    }
+}
