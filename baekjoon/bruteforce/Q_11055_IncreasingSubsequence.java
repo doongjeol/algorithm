33e8af62285f7a7373c5c857a3bcbc731e959368
김다인 <dainkim@sparrowfasoo.com>
2020-12-17 17:35:08 +0900

    12월 17일

--- baekjoon/bruteforce/Q_11055_IncreasingSubsequence.java
+++ baekjoon/bruteforce/Q_11055_IncreasingSubsequence.java
@@ -6,12 +6,20 @@ public class Q_11055_IncreasingSubsequence {
     public static int solution(int[] a){
         int result = 0;
 
-        for (int index = 0; index < a.length; index++) {
-            int start = a[index];
-            for (int index2 = 1; index2 < a.length; index2++) {
-                
-            }
+        for (int start = 0; start < a.length; start++) {
+            int temp = a[start];
+            int sum = a[start];
+            for (int index = start; index < a.length-1; index++) {
+                for (int indexTwo = index; indexTwo < a.length - 1; indexTwo++) {
 
+                }
+                if (temp < a[index + 1]) {
+                    sum += a[index + 1];
+                    temp = a[index + 1];
+                }
+            }
+            result = Math.max(sum, result);
+            System.out.println(result);
 
         }
 
@@ -30,7 +38,7 @@ public class Q_11055_IncreasingSubsequence {
             a[i] = Integer.parseInt(st.nextToken());
         }
 
-        bw.write(a +"\n");
+        bw.write(solution(a)+"\n");
 
         br.close();
         bw.close();
