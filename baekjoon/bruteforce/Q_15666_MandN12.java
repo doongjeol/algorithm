c5176754da9ec538886cd9a21260fd3e2c05ead1
김혜진 <cd8025@nate.com>
2022-04-01 18:49:14 +0900

    15666 M과 N 12 다시 풀어봐야한다

---
+++ baekjoon/bruteforce/Q_15666_MandN12.java
@@ -0,0 +1,23 @@
+package baekjoon.bruteforce;
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Q_15666_MandN12 {
+    public static void solution(){
+
+    }
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        
+        for (int i = 0; i < n; i++) {
+
+        }
+        br.close();
+        bw.close();
+    }
+}
