003742e0fa7fa9b96ce200ad7967393e3f30a535
김혜진 <cd8025@nate.com>
2022-04-10 13:57:12 +0900

    15665

--- baekjoon/bruteforce/Q_15665_MandN11.java
+++ baekjoon/bruteforce/Q_15665_MandN11.java
@@ -1,21 +1,30 @@
 package baekjoon.bruteforce;
 import java.io.*;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class Q_15665_MandN11 {
     public static int n,m;
     public static int[] num;
-    public static void permutation(int[] arr, boolean[] selected, int depth, BufferedWriter bw) throws IOException{
+    public static HashSet<String> hashSet = new HashSet<>();
+    public static void permutation(int[] arr, int depth, BufferedWriter bw) throws IOException{
         if (depth == m) {
+            String str = "";
+            for (int i = 0; i < arr.length; i++) {
+                str += arr[i] +" ";
+            }
+
+            if(!hashSet.contains(str)){
+                hashSet.add(str);
+                bw.write(str+"\n");
+            }
             return;
         }
 
         for (int i = 0; i < n; i++) {
-            if(selected[i]) continue;
-            selected[i] = true;
             arr[depth] = num[i];
-            permutation(arr, selected, depth + 1, bw);
-            selected[i] = false;
+            permutation(arr, depth + 1, bw);
         }
     }
     public static void main (String[] args) throws IOException {
@@ -32,6 +41,9 @@ public class Q_15665_MandN11 {
         for (int i = 0; i < n; i++) {
             num[i] = Integer.parseInt(st.nextToken());
         }
+
+        Arrays.sort(num);
+        permutation(new int[m], 0, bw);
         br.close();
         bw.close();
     }
