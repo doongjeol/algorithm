018ea200a0b7e9f500a3a379bffdfd2820f99268
kimdainn <dainkim@sparrowfasoo.com>
2022-05-16 20:38:19 +0900

    a

---
+++ baekjoon/string/Q_1316_GroupWordChecker.java
@@ -0,0 +1,25 @@
+package baekjoon.string;
+
+import java.io.*;
+
+public class Q_1316_GroupWordChecker {
+    public static int result = 0;
+    public static void solution(String str) {
+        int[] alphabet = new int[26];
+        
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            String str = br.readLine();
+        }
+
+        br.close();
+        bw.close();
+    }
+}
