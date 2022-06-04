fd64558b0883f1294065d478bd9c5bb8e7f8db7e
kimdainn <cd8025@nate.com>
2022-06-04 22:27:52 +0900

    1100 하얀칸

--- baekjoon/string/Q_1100_WhiteArea.java
+++ baekjoon/string/Q_1100_WhiteArea.java
@@ -1,7 +1,7 @@
 package baekjoon.string;
 import java.io.*;
 
-public class Q_WhiteArea {
+public class Q_1100_WhiteArea {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
