d210bcee5552145b701462a62a8a297711f1da60
kimdainn <cd8025@nate.com>
2022-07-19 23:44:16 +0900

    1068 푸는중

---
+++ baekjoon/tree/Q_1068_Tree.java
@@ -0,0 +1,17 @@
+package baekjoon.tree;
+import java.io.*;
+
+public class Q_1068_Tree {
+    public static class Node {
+        
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        br.close();
+        bw.close();
+    }
+}
