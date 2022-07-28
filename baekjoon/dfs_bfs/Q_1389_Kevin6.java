79fe9512a9a877e9fb9d91c317cd06378e6b5241
kimdainn <cd8025@nate.com>
2022-07-28 21:49:01 +0900

    1389 백준

---
+++ baekjoon/dfs_bfs/Q_1389_Kevin6.java
@@ -0,0 +1,78 @@
+package baekjoon.dfs_bfs;
+import java.util.*;
+
+public class Q_1389_Kevin6 {
+    public static LinkedList<Integer>[] adjList;
+    public static boolean[] visited;
+    public static int[] check; // BFS 수행시 이동 거리(탐색 너비) 측정
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+        adjList = new LinkedList[N+1];
+        for(int i= 1; i < N+1; i++) {
+            adjList[i] = new LinkedList<>();
+        }
+
+        for(int i = 0; i < M; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            adjList[a].add(b);
+            adjList[b].add(a);
+        }
+
+        int min = Integer.MAX_VALUE;
+        int ans = 0;
+
+        for(int i = 1; i < N+1; i++) {
+            int sum = 0;
+            for(int j = 1; j < N+1; j++) {
+                if(j != i) {
+                    visited = new boolean[N+1];
+                    check = new int[N+1];
+                    sum += bfs(i, j, 0);
+                }
+            }
+
+            if(sum < min) {
+                ans = i;
+                min = Math.min(min, sum);
+            }
+        }
+
+        System.out.println(ans);
+    }
+
+    public static int bfs(int start, int find, int cnt) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(start);
+        visited[start] = true;
+
+        while(!queue.isEmpty()) {
+            int cur = queue.poll();
+
+            Iterator<Integer> iter = adjList[cur].iterator();
+            while(iter.hasNext()) {
+
+                int next = iter.next();
+
+                if(!visited[next]) {
+                    // BFS를 수행하며 한 칸씩 너비를 늘려나갈때마다 1씩 추가됨
+                    check[next] = check[cur] + 1; // 최소 거리 측정을 위함 ex) 1 -> 5의 경우 1,3,4,5가 아니라 1,4,5를 거르기 위함.
+
+                    if(next == find) { // 종료 정점
+                        return check[next]; // 최소 친구 단계의 합
+                    }
+
+                    visited[next] = true;
+                    queue.add(next);
+                }
+            }
+        }
+        return 0;
+    }
+}
+
